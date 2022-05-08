bb14d714739529bc302ad662a9a13b6ca2569dbc
zhongrb <zhongrb0@sina.com>
2022-05-08 15:42:21 +0800

    打包后的文件

---
+++ dist/assets/groupbuy.6bae4cd8.js
@@ -0,0 +1,1 @@
+import{_ as e,o as r,d as c}from"./index.2d68104a.js";const o={};function n(t,a){return r(),c("div",null," \u6211\u662F\u56E2\u8D2D\u8BA2\u5355\u5185\u5BB9 ")}var s=e(o,[["render",n]]);export{s as default};
